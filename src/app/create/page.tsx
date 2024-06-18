22ae5147f47f182d6164f8b4b2790d4ac00e5a7c
YanYuan <yan950414@gmail.com>
2024-06-18 22:38:04 +0800

    feat: create name

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -7,13 +7,40 @@ import { extractPublicKey } from '@/core/utils';
 import { useRouter } from 'next/navigation';
 import { Container } from '@/components/Container';
 import logoImg from '@/assets/moss.png';
+import { Modal } from '@/components/Modal';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { useState } from 'react';
+
+const CreateWalletModal = ({ onConfirm }: { onConfirm: (v: string) => void }) => {
+  const [name, setName] = useState('');
+  return (
+    <Modal
+      confirmButtonProps={{
+        disabled: !name
+      }}
+      onConfirm={() => {
+        onConfirm(name);
+      }}
+      header={'Create Wallet'}
+      trigger={<Button className={'w-full'}>Create New Wallet</Button>}
+    >
+      <div className={'space-y-6'}>
+        <div className="space-y-2">
+          <Label>Wallet Name</Label>
+          <Input placeholder={'Wallet Name'} value={name} onChange={(e) => setName(e.target.value)} />
+        </div>
+      </div>
+    </Modal>
+  );
+};
 
 export default function Home() {
   const router = useRouter();
 
-  const handleClick = async () => {
+  const handleCreate = async (name: string) => {
     try {
-      const options = getPasskeyOptions();
+      const options = getPasskeyOptions(name);
       console.log(options);
       const cred = await navigator.credentials.create({
         publicKey: options
@@ -51,13 +78,20 @@ export default function Home() {
       <div className={'h-[85vh] text-center flex flex-col items-center justify-center gap-6 '}>
         <img src={logoImg.src} className={'w-12 h-12 absolute left-5 top-5'} alt="" />
         <div className={'mb-8'}>
-          <h1 className="text-4xl font-bold text-primary">Welcome to Moss</h1>
+          <h1 className="text-4xl font-bold">
+            Welcome to{' '}
+            <span className={'bg-clip-text text-transparent bg-gradient-to-b from-[#FF72E1] to-[#F54C7A]'}>Moss</span>
+          </h1>
           <p className="text-lg text-gray-500 mt-4">one-click to unlock web3</p>
         </div>
-        <Button onClick={handleClick} className={'w-full'}>
-          Create New Wallet
-        </Button>
-        <Button onClick={handleClick} className={'w-full'}>
+        <CreateWalletModal onConfirm={handleCreate} />
+        <Button
+          onClick={() => {
+            console.log('login with passkey');
+            alert('wait for implementation');
+          }}
+          className={'w-full'}
+        >
           Login With Passkey
         </Button>
       </div>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,49 +1,52 @@
-import {nanoid} from "nanoid";
+import { nanoid } from 'nanoid';
 
-export const getPasskeyOptions = () => {
-    return {
-        "challenge": Buffer.from(nanoid(20)),
-        "rp": {
-            "name": "moss",
-            "id": window.location.hostname
-        },
-        "user": {
-            "id": Buffer.from("GOVsRuhMQWNoScmh_cK02QyQwTolHSUSlX5ciH242Y4"),
-            "name": "moss",
-            "displayName": "moss"
-        },
-        "pubKeyCredParams": [
-            {
-                "alg": -7,
-                "type": "public-key" as const
-            }
-        ],
-        "timeout": 60000,
-        "excludeCredentials": [
-        ],
-        "authenticatorSelection": {
-            "authenticatorAttachment": "platform" as const,
-            "requireResidentKey": true,
-            "residentKey": "required" as const
-        },
-        "extensions": {
-            "credProps": true
-        }
+export const getPasskeyOptions = (name: string) => {
+  return {
+    challenge: Buffer.from(nanoid(20)),
+    rp: {
+      name: name,
+      id: window.location.hostname
+    },
+    user: {
+      id: Buffer.from(nanoid(16)),
+      name: name,
+      displayName: name
+    },
+    pubKeyCredParams: [
+      {
+        alg: -7,
+        type: 'public-key' as const
+      }
+    ],
+    timeout: 60000,
+    excludeCredentials: [],
+    authenticatorSelection: {
+      authenticatorAttachment: 'platform' as const,
+      requireResidentKey: true,
+      residentKey: 'required' as const
+    },
+    extensions: {
+      credProps: true
     }
-}
+  };
+};
 
 export function base64url_encode(buffer: ArrayBuffer): string {
-    return btoa(Array.from(new Uint8Array(buffer), b => String.fromCharCode(b)).join(''))
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=+$/, '');
+  return btoa(Array.from(new Uint8Array(buffer), (b) => String.fromCharCode(b)).join(''))
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '');
 }
 
 export function base64url_decode(value: string): ArrayBuffer {
-    const m = value.length % 4;
-    return Uint8Array.from(atob(
-        value.replace(/-/g, '+')
-            .replace(/_/g, '/')
-            .padEnd(value.length + (m === 0 ? 0 : 4 - m), '=')
-    ), c => c.charCodeAt(0)).buffer
+  const m = value.length % 4;
+  return Uint8Array.from(
+    atob(
+      value
+        .replace(/-/g, '+')
+        .replace(/_/g, '/')
+        .padEnd(value.length + (m === 0 ? 0 : 4 - m), '=')
+    ),
+    (c) => c.charCodeAt(0)
+  ).buffer;
 }
